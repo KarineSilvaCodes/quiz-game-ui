4816c727e4d02d988006182c69b5d82ba6a7d517
Karine-Codes <kahdev404@gmail.com>
2025-10-18 11:28:03 -0300

    Adicionar o arquivo QuizGame.java

---
+++ QuizGame.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class QuizGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int score = 0; 
+    }
+}
