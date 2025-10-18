edead11f0d6abaab7a384c6dc1a380bbf7dad11a
Karine-Codes <kahdev404@gmail.com>
2025-10-18 14:33:56 -0300

    Lógica funcional do quiz Naruto

--- QuizGame.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class QuizGame {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        int score = 0; 
-    }
-}
